0a5bddaa1f6bd3a78e02bb6c7ab5c60ef1e6e3ca
gbyfns <101527892+gbyfns@users.noreply.github.com>
2022-10-28 11:33:49 +1300

    Add files via upload

--- 01_square_rectangle_function_v1.py
+++ 01_square_rectangle_function_v1.py
@@ -4,6 +4,7 @@ square rectangle component
 v1 - calculate area and perimeter for square and rectangle function
 '''
 
+
 def program_start():
     t
 
@@ -22,6 +23,7 @@ def rectangle_area():
 
         if answer_rectangle_area == rectangle_area_calc:
             print("correct, well done!")
+
             answer_rectangle_area == "correct!"
             answer_area_rectangle == +1
             rectangle_perimeter()
@@ -29,6 +31,7 @@ def rectangle_area():
 
         else:
             print("incorrect. the correct answer is ", rectangle_area_calc)
+
             answer_rectangle_area == "incorrect."
             answer_area_rectangle == +1
             rectangle_perimeter()
@@ -36,17 +39,19 @@ def rectangle_area():
 
 def rectangle_perimeter():
     while answer_perimeter_rectangle == 0:
+
         length = float(input("input the length of the rectangle: "))
         height = float(input("input the height of the rectangle: "))
         rectangle_perimeter_calc = length * 2 + height * 2
         rectangle_perimeter_int = int(rectangle_perimeter_calc)
-        # (f'__Perimeter__\n P = (L×2)+(H×2)\n A = {length}×2+{height}×2 \n A = {length * 2 + height * 2}')
         answer_rectangle_perimeter = int(input("input the answer for the perimeter: "))
 
         if answer_rectangle_perimeter == rectangle_perimeter_int:
             print("correct, well done!")
+
             answer_rectangle_perimeter == "correct"
             answer_perimeter_rectangle == answer_perimeter_rectangle + 1
+
             print("thanks for playing")
             quit()
 
@@ -54,8 +59,10 @@ def rectangle_perimeter():
 
         else:
             print("incorrect. The correct answer is ", rectangle_perimeter_int)
+
             answer_rectangle_perimeter == "incorrect"
             answer_perimeter_rectangle == answer_perimeter_rectangle + 1
+
             print("thanks for playing")
             quit()
 
